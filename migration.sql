4114c98803c57e89d09018ef23d88b7c98c99afd
Srikanth Katta <srikantkatta2207@gmail.com>
2018-03-09 20:20:37 +0530

    [Srikanth] added migration script for dhis 2.29

---
+++ migration.sql
@@ -0,0 +1,27 @@
+/*
+	Migration script to include lastupdated,lastupdateby and complted columns.
+	It will add these three columns to existing database.
+*/
+
+alter table completedatasetregistration 
+add lastupdated date, add lastupdatedby text,add completed boolean;
+
+create or replace function migrateCompletenessTable() returns void as $$ 
+declare complete RECORD;
+declare usrrow integer;
+begin
+for complete in select * from completedatasetregistration
+loop
+	select usr.userid into usrrow from users usr where usr.username = complete.storedby limit 1;
+	update completedatasetregistration set completed='t',lastupdated=complete.date,lastupdatedby=usrrow
+			where datasetid = complete.datasetid and
+			  	  periodid=complete.periodid and
+			  	  sourceid=complete.sourceid and
+			  	  attributeoptioncomboid=complete.attributeoptioncomboid;
+
+end loop;
+end;
+$$ language plpgsql;
+select migrateCompletenessTable();
+
+alter table completedatasetregistration alter column lastupdatedby set not null;
